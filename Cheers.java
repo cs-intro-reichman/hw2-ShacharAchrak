66a0e31f41521b50e6529a13bdec02ad7b01647c
ShacharAchrak <shachar.achrak@post.runi.ac.il>
2024-11-19 00:33:47 +0200

    Cheers

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,27 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String name = args[0];
+                int times = Integer.parseInt(args[1]);
+
+               for (int i = 0; i < name.length(); i++) {
+                   System.out.print("Give me ");
+                  char c =name.charAt(i);
+                   if(c == 'A' || c == 'E'|| c == 'F' || c == 'H' || c == 'I' || c == 'L' || c == 'M' || c == 'N' || c == 'O' || c == 'R' || c == 'S' || c == 'X'){
+                        
+                        System.out.print("an ");
+                
+                
+                } else {
+                                System.out.print("a ");
+                        }
+                        
+                        System.out.println(name.charAt(i) + ": " +name.charAt(i) +"!");
+                }
+
+               for(int j=0; j<times; j++){
+                    System.out.println(name + " !!!");
+                   }
+               
         }
 }
