eb626fed478c65e68eb3c4f888bc372a674f668f
ShacharAchrak <shachar.achrak@post.runi.ac.il>
2024-11-19 00:48:27 +0200

    Cheers

--- Cheers.java
+++ Cheers.java
@@ -21,7 +21,7 @@ public class Cheers {
                System.out.println("What does that spell?");
                
                for(int j=0; j<times; j++){
-                    System.out.println(name + " !!!");
+                    System.out.println(name + "!!!");
                    }
                
         }
