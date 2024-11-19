f47a87864190f9952bba234ac00992a0e4bfccac
ShacharAchrak <shachar.achrak@post.runi.ac.il>
2024-11-19 16:06:11 +0200

    Collatz.java

--- Collatz.java
+++ Collatz.java
@@ -4,28 +4,25 @@ public class Collatz {
 	    int seed = Integer.parseInt(args[0]);
 		String mode = args[1];
 
-	    if (mode.equals("v")) {
-		   for (int i = 1; i <= seed; i ++) {
-			int current = i;
-            int step = 0;
-			
-			System.out.print(current + " ");
-				
-				while (current != 1){
-	        	   if(current % 2 == 0){
-					current = current / 2;
-				} else{
-					 current = current * 3 + 1;
-				}
-				System.out.print(current + " ");
-				step ++;
-			    }
-				
-		   System.out.println("(" + (step + 1) + ")");
-           }
+        if (mode.equals("v")) {
+            for (int i = 1; i <= seed; i++) {
+                int current = i;
+                int steps = 0;
 
-		   System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
-    	}
+                System.out.print(current + " ");
+                while (current != 1) {
+                    if (current % 2 == 0) {
+                        current = current / 2;
+                    } else {
+                        current = current * 3 + 1;
+                    }
+                    System.out.print(current + " "); 
+                    steps++;
+                }
+                System.out.println("(" + (steps + 1) + ")");
+            }
+            System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
+        }
 
 	if (mode.equals("c")) {
 		for (int i = 1; i <= seed; i ++) {
