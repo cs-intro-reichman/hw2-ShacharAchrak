d796a773fe186a4efaf24a02fa3602c434fb2776
ShacharAchrak <shachar.achrak@post.runi.ac.il>
2024-11-19 11:33:03 +0200

    CalcPi

--- CalcPi.java
+++ CalcPi.java
@@ -2,5 +2,18 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
+		int terms = Integer.parseInt(args[0]);
+		double piApproximation = 0;
+       
+		for (int i = 0; i < terms; i++) {
+            double term = Math.pow(-1, i) / (2 * i + 1); 
+            piApproximation += term;
+        }
+
+        piApproximation *= 4;
+
+System.out.println("pi according to Java: " + Math.PI);
+System.out.println("pi, approximated: " + piApproximation);
+
 	}
 }
